bd815552515bb9414a820f30c8f5713ecac7c8ab
sim <sim@simdemac-mini.local>
2018-02-02 09:12:03 +0800

    Initial Commit

---
+++ Swift_Runtime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift_Runtime
+//
+//  Created by sim on 2018/2/2.
+//  Copyright © 2018年 wanglupeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
